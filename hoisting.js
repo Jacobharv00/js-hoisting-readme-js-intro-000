7e0c7fd5ff18ce4d57a4e888f634e04fc66b70d8
Jacob Harvey <jacobharv00@gmail.com>
2021-01-22 19:55:50 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -9,11 +9,9 @@ function callMe() {
 }
 
 function crazy() {
-
+var thisIsCrazy = function() {
   // fix the code in here:
   thisIsCrazy();
-
-  var thisIsCrazy = function (){
     console.log("hey!!!")
     }
   }
